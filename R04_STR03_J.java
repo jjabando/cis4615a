771fb0d7f4e7bfed30fd73e6f31b5385d5e33579
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-11-01 17:51:13 -0400

    Updated R04
    Got rid of convertInt() function and just put code into main(). main() handles all testing anyway

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,26 +1,21 @@
 /*
+ * Jesse Jamieson Abando
  * Rule 04: Characters and Strings
  * STR03-J. Do not encode noncharacter data as a string
  */
-package cis4615work;
 
 import java.math.BigInteger;
 
-/**
- *
- * @author JJ
- */
 public class R04_STR03_J {
-    private void convertInt(){
+    public static void main(String[] args){
+        System.out.println("Original value: 530500452766");
+        
         BigInteger x = new BigInteger("530500452766");
         String s = x.toString();  // Valid character data
         byte[] byteArray = s.getBytes();
         String ns = new String(byteArray);
         x = new BigInteger(ns);
+        
         System.out.println("Compliant function value: " + x);
-    }
-    public void execute(){
-        System.out.println("Original value: 530500452766");
-        convertInt();
     }
 }
