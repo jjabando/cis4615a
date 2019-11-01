d7849350202d71fef5e6603c6fb06de947ea05a9
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-11-01 17:18:50 -0400

    Updated Rule 06

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,14 +1,11 @@
 /*
+ * Jesse Jamieson Abando
  * Rule 06. Methods (MET)
  * MET01-J. Never use assertions to validate method arguments
  */
-package cis4615work;
 
-/**
- *
- * @author JJ
- */
-public class R06_MET01_J {
+public class R06_MET01_J{
+    
     public static int getAbsAdd(int x, int y){
         if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE){
             throw new IllegalArgumentException();
@@ -20,7 +17,19 @@ public class R06_MET01_J {
         }
         return absX + absY;
     }
-    void execute(){
-        System.out.println(getAbsAdd(100,200));
+    public static void main(String[] args) {
+        int a = 17;       
+        System.out.println("a = " + a);
+        int b = 42;       
+        System.out.println("b = " + b);       
+        int c = getAbsAdd(a, b);       
+        System.out.println("c = " + c);
+        
+        a = Integer.MIN_VALUE; //used for testing secure version       
+        System.out.println("a = " + a);       
+        b = Integer.MAX_VALUE;       
+        System.out.println("b = " + b);       
+        c = getAbsAdd(a, b);       
+        System.out.println("c = " + c);
     }
 }
