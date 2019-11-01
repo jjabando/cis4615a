32b597b9fcf9f120d2830e08a7eaf6cd585142fe
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-11-01 17:54:32 -0400

    Updated Rule 03
    changed to execute() to main()

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,21 +1,17 @@
 /*
+ * Jesse Jamieson Abando
  * Rule 03: Numeric Types and Operations (NUM)
  * NUM03-J. Use integer types that can fully represent the possible range of unsigned data
  */
-package cis4615work;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 
-/**
- *
- * @author JJ
- */
 public class R03_NUM03_J {
     public static long getInteger(DataInputStream is) throws IOException{
         return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
     }
-    void execute(){
+    public static void main(String[] args){
         DataInputStream dis = new DataInputStream(System.in);
         try{
             System.out.println(getInteger(dis));
