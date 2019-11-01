bd86a4eb9dba484be365a0f96c24a2133114a029
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-11-01 18:40:21 -0400

    Updated Rule 02
    changed execute to main()

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,22 +1,21 @@
 /*
+ * Jesse Jamieson Abando
  * Rule 02: Expressions (EXP)
  * XP00-J. Do not ignore values returned by methods
  */
-package cis4615work;
+
 import java.io.File;
-/**
- *
- * @author JJ
- */
+
 public class R02_XP00_J{
-    public void deleteFile(){
+    public static void deleteFile(){
         File someFile = new File("someFileName.txt");
         // Do something with someFile
         if (!someFile.delete()) {
         // Handle failure to delete the file
         }
     }
-    void execute(){
+    public static void main(String[] args){
+        System.out.println("Creating and correctly deleting a file...");
         deleteFile();
     }
 }
