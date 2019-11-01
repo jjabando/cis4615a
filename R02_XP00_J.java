6962d6b79d66d604ea52363ab5f3648b958e73d5
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-10-31 20:59:15 -0400

    Updated Rule 02
    Compliant Code

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -12,7 +12,9 @@ public class R02_XP00_J{
     public void deleteFile(){
         File someFile = new File("someFileName.txt");
         // Do something with someFile
-        someFile.delete();
+        if (!someFile.delete()) {
+        // Handle failure to delete the file
+        }
     }
     void execute(){
         deleteFile();
