fe1b8eeb48f8492a5b520318af96fcdba6e22a5b
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-11-01 21:19:17 -0400

    Updated Rule 07
    Logger implemented

--- R07_ERR02_J.java
+++ R07_ERR02_J.java
@@ -5,13 +5,17 @@
  */
 
 import java.io.IOException;
+import java.util.function.Supplier;
+import java.util.logging.*;
 
 public class R07_ERR02_J{
+    static Logger logger = Logger.getLogger(R07_ERR02_J.class.getName());
     public static void main(String[] args){
         try {
             // ...
         } catch (SecurityException se) {
             System.err.println(se);
+            logger.log(Level.SEVERE, (Supplier<String>) se);
             // Recover from exception
         }
     }
